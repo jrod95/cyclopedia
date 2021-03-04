dba5f13c16e1195029bd572b2efcf6e7ade87939
kmarsk <76917225+kmarsk@users.noreply.github.com>
2021-03-04 20:02:53 +0100

    Update and rename bicycle_controller_test.rb to bicycles_controller_test.rb

--- test/controllers/bicycles_controller_test.rb
+++ test/controllers/bicycles_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class BicycleControllerTest < ActionDispatch::IntegrationTest
+class BicyclesControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
