05bf5abaa13f409f8ac3e149ae53015efbf3088c
Juan Rodriguez <jrcam95@gmail.com>
2021-03-04 15:48:41 -0300

    Made bicycle controller

---
+++ app/controllers/bicycle_controller.rb
@@ -0,0 +1,2 @@
+class BicycleController < ApplicationController
+end
---
+++ test/controllers/bicycle_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class BicycleControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
