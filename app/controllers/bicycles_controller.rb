67e2ae54a6ed04beb5296d17406469867427fff1
Marta Kask <marta.m.kask@gmail.com>
2021-03-04 22:17:11 +0100

    authentication policies and conttrollers done, missing views

--- app/controllers/bicycles_controller.rb
+++ app/controllers/bicycles_controller.rb
@@ -1,2 +1,59 @@
 class BicyclesController < ApplicationController
+
+ before_action :set_bicycle, only: [:show, :edit, :update, :destroy]
+
+def new
+  @bicycle = Bicycle.new
+  authorize @bicycle
 end
+
+def index
+  @bicycles = policy_scope(Bicycle)
+end
+
+
+def create
+  @bicycle = Bicycle.new(bicycle_params)
+  authorize @bicycle
+  @bicycle.user = current_user
+
+  if @bicycle.save
+    redirect_to @bicycle, notice: "Bicycle was successfully created"
+
+  else
+    render :new
+  end
+end
+
+def show
+  authorize @bicycle
+end
+
+def update
+  authorize @bicycle
+  if @bicycle.update(bicycle_params)
+  redirect_to @bicycle, notice "Bicycle was updated"
+  else
+  render :edit
+  end
+end
+
+def destroy
+  @bicycle.destroy
+  authorize @bicycle
+end
+
+
+private
+
+    # Use callbacks to share common setup or constraints between actions.
+    def set_bicycle
+      @bicycle = Bicycle.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def bicycle_params
+      params.require(:type, :brand, :model, :size).permit(:type, :brand, :model, :size)
+    end
+end
+
